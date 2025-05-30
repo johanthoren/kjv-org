90a9529f33796f74e2608d87a98a142db475f394
Johan Thoren <johan@thoren.xyz>
2025-05-30 12:17:05 +0200

    Extract authorship notes to metadata format - fixes #4

--- generate_single_file.sh
+++ generate_single_file.sh
@@ -1,41 +1,56 @@
 #!/bin/sh
 
-usage="$(basename "$0") [-h] -- Combine all org files in the current directory."
+usage="$(basename "$0") [-h] [-f|--force] -- Combine all org files in the current directory."
 
-if [ -n "$1" ]; then
+FORCE=0
+
+while [ "$#" -gt 0 ]; do
     case $1 in
-        -h)
+        -h|--help)
             echo "$usage"
+            echo ""
+            echo "Options:"
+            echo "  -h, --help    Show this help message"
+            echo "  -f, --force   Overwrite existing KJV.org without prompting"
             exit 0
             ;;
+        -f|--force)
+            FORCE=1
+            ;;
         *)
-            echo "Invalid option or argument."
+            echo "Invalid option: $1"
             echo "Usage:"
             echo "$usage"
             exit 1
             ;;
     esac
-fi
+    shift
+done
 
 OUT="KJV.org"
 
 if [ -f "$OUT" ]; then
-    read -r -p "The file $OUT already exits. Overwrite? [y/N] " input
+    if [ "$FORCE" -eq 1 ]; then
+        echo "Force mode: Removing existing $OUT."
+        rm "$OUT" || exit 1
+    else
+        read -r -p "The file $OUT already exits. Overwrite? [y/N] " input
 
-    case $input in
-        [yY][eE][sS]|[yY])
-            echo "Removing existing $OUT."
-            rm "$OUT" || exit 1
-            ;;
-        [nN][oO]|[nN]|"")
-            echo "Aborting."
-            exit 0
-            ;;
-        *)
-            echo "Invalid input..."
-            exit 1
-            ;;
-    esac
+        case $input in
+            [yY][eE][sS]|[yY])
+                echo "Removing existing $OUT."
+                rm "$OUT" || exit 1
+                ;;
+            [nN][oO]|[nN]|"")
+                echo "Aborting."
+                exit 0
+                ;;
+            *)
+                echo "Invalid input..."
+                exit 1
+                ;;
+        esac
+    fi
 fi
 
 echo "Combining all source files."
@@ -46,20 +61,53 @@ for file in *.org; do
     fi
 done
 
-echo "Moving all chapter markers two steps to the right."
-sed -i'' 's/^\*\ /\*\*\*\ /g' "$OUT"
-echo "Converting TITLEs."
-sed -i'' 's/^#+TITLE\:\ /\*\*\ /g' "$OUT"
-echo "Adding OT and NT markers."
-sed -i'' '1i\* Old Testament' "$OUT"
-sed -i'' '/^\*\* Matthew/i\* New Testament' "$OUT"
-sed -i'' '1i\\' "$OUT"
-echo "Adding file header."
-sed -i'' '1i\#+LaTeX: \\setcounter{secnumdepth}{0}' "$OUT"
-sed -i'' '1i\#+OPTIONS: author:nil date:nil toc:2' "$OUT"
-sed -i'' '1i\#+STARTUP: overview' "$OUT"
-sed -i'' '1i\#+SUBTITLE: King James Version (Authorized)' "$OUT"
-sed -i'' '1i\#+TITLE: The Holy Bible' "$OUT"
+echo "Processing file with awk..."
+awk '
+BEGIN {
+    # Print file header
+    print "#+TITLE: The Holy Bible"
+    print "#+SUBTITLE: King James Version (Authorized)"
+    print "#+STARTUP: overview"
+    print "#+OPTIONS: author:nil date:nil toc:2"
+    print "#+LaTeX: \\setcounter{secnumdepth}{0}"
+    print ""
+    print "* Old Testament"
+    print ""
+    ot_printed = 1
+    nt_printed = 0
+}
+
+# Convert chapter markers from * to ***
+substr($0,1,2) == "* " { 
+    gsub(/^[*] /, "*** ")
+    print
+    next 
+}
+
+# Convert titles from #+TITLE: to **
+substr($0,1,8) == "#+TITLE:" { 
+    sub(/^#\+TITLE: /, "** ")
+    # Check if this is Matthew (first NT book) and we haven'\''t printed NT header yet
+    if ($0 ~ /Matthew/ && !nt_printed) {
+        print "* New Testament"
+        print ""
+        nt_printed = 1
+    }
+    print
+    next 
+}
+
+# Convert author notes from #+AUTHOR_NOTE: to //
+substr($0,1,14) == "#+AUTHOR_NOTE:" { 
+    sub(/^#\+AUTHOR_NOTE: /, "//")
+    print
+    next 
+}
+
+# Print all other lines as-is
+{ print }
+' "$OUT" > "${OUT}.tmp" && mv "${OUT}.tmp" "$OUT"
+
 echo "All done."
 
 exit 0
